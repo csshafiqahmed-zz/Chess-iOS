cdc3918839ad68eb8124b600f04024925ac96559
Satish Boggarapu <satishbogg9@gmail.com>
2019-02-05 09:37:14 -0500

    Initial Commit

---
+++ Checkers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checkers
+//
+//  Created by Satish Boggarapu on 2/5/19.
+//  Copyright © 2019 SatishBoggarapu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
